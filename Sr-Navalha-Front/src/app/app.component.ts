3853465259d9079e7cac06e8f9a7dadfe2fcb795
Anisberto Reis <anisbertoos@gmail.com>
2021-05-04 23:23:33 -0300

    [Anisberto Reis] criacao dos componentes: login, header, home, footer

--- Sr-Navalha-Front/src/app/app.component.ts
+++ Sr-Navalha-Front/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'Sr-Navalha-Front';
+  title = 'Sr Navalha';
 }
--- Sr-Navalha-Front/src/app/app.module.ts
+++ Sr-Navalha-Front/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './view/login/login.component';
+import { RecuperarSenhaComponent } from './view/recuperar-senha/recuperar-senha.component';
+import { HomeComponent } from './view/home/home.component';
+import { FooterComponent } from './view/footer/footer.component';
+import { HeaderComponent } from './view/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RecuperarSenhaComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
