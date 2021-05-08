5d885c9ed01a499d149d239aec4cb7d5216f7f55
jhonathandosreis <jhonathandosreis@hotmail.com>
2021-05-08 00:47:18 -0300

    [JHONATHAN DOS REIS] - Alterações no template da home - login - recuperar senha

--- Sr-Navalha-Front/src/app/app-routing.module.ts
+++ Sr-Navalha-Front/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './view/home/home.component';
 import { LoginComponent } from './view/login/login.component';
+import { RecuperarSenhaComponent } from './view/recuperar-senha/recuperar-senha.component';
 
 const routes: Routes = [
   {path:'',component: HomeComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'recuperarSenha',component:RecuperarSenhaComponent}
 ];
 
 @NgModule({
