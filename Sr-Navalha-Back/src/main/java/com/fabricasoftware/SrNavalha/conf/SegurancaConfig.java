c5e208811885231bf55cb5230dbecd7dbb7835d8
jhonathandosreis <jhonathandosreis@hotmail.com>
2021-05-18 16:45:15 -0300

    [JHONATHAN DOS REIS] - Implementação Swagger

--- Sr-Navalha-Back/src/main/java/com/fabricasoftware/SrNavalha/conf/SegurancaConfig.java
+++ Sr-Navalha-Back/src/main/java/com/fabricasoftware/SrNavalha/conf/SegurancaConfig.java
@@ -1,8 +1,11 @@
 package com.fabricasoftware.SrNavalha.conf;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -11,6 +14,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableWebSecurity
+@Configuration
 @EnableAuthorizationServer
 @EnableResourceServer
 public class SegurancaConfig extends WebSecurityConfigurerAdapter {
@@ -29,5 +33,24 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/login**").permitAll()
+                .antMatchers(HttpMethod.GET, "/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/**").permitAll()
+                .antMatchers(HttpMethod.PATCH, "/**").permitAll()
+                .antMatchers(HttpMethod.PUT, "/rest/**").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/**").permitAll();
     }
 }
