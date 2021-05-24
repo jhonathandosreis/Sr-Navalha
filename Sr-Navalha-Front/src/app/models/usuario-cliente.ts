016f242674cf742dbd003e39453e94f0de13946b
jhonathandosreis <jhonathandosreis@hotmail.com>
2021-05-24 01:13:06 -0300

    [JHONATHAN DOS REIS] - Cors reemplementado

---
+++ Sr-Navalha-Front/src/app/models/usuario-cliente.ts
@@ -0,0 +1,13 @@
+export class UsuarioCliente {
+    id: any;
+    nome: any;
+    telefone: any;
+    email: any;
+    dataNascimento: any;
+    cpf: any;
+    tipo: any;
+    agendamento: any;
+    endreco: any;
+    credencial: any;
+    perfil: any;
+}
