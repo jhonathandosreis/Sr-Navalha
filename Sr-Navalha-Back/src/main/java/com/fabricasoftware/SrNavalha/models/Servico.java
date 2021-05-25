337b0de32ecef01cf2459edff551f726f6daa687
Gustavo-404 <gustavogabriel77849@gmail.com>
2021-05-25 14:17:56 -0300

    imageUrl em Serviços

--- Sr-Navalha-Back/src/main/java/com/fabricasoftware/SrNavalha/models/Servico.java
+++ Sr-Navalha-Back/src/main/java/com/fabricasoftware/SrNavalha/models/Servico.java
@@ -22,6 +22,7 @@ public class Servico {
     private String nome;
     private String descricao;
     private Double valor;
+    private String imageUrl;
     @ManyToOne
     private UsuarioBarbeiro usuarioBarbeiro;
 
