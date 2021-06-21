5595dca3f9a55d57902046c2ebe5bc5fda8e8f70
anisberto Reis <anisbertoos@gmail.com>
2021-06-21 20:05:47 -0300

    ajuste de agendamento

--- Sr-Navalha-Front/src/app/app-routing.module.ts
+++ Sr-Navalha-Front/src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { NotFoundComponent } from './view/not-found/not-found.component';
-import { AuthGuard } from './auth.guard';
 import { UpdateAgendamentoComponent } from './view/agendamento/update-agendamento/update-agendamento.component';
 import { SpinnerComponent } from './view/spinner/spinner.component';
 import { AgendamentosComponent } from './view/agendamento/agendamentos/agendamentos.component';
@@ -16,38 +15,34 @@ import { TelaBarbeiroComponent } from './view/tela-barbeiro/tela-barbeiro.compon
 import { TelaClienteComponent } from './view/tela-cliente/tela-cliente.component';
 import { ServicoListComponent } from './view/servico/servico-list/servico-list.component';
 import { PerfilClienteComponent } from './view/perfil-cliente/perfil-cliente.component';
-
 import { ServicoListBarbeiroComponent } from './view/servico/servico-list-barbeiro/servico-list-barbeiro.component';
-
 import { TelaClienteAdminComponent } from './view/tela-cliente/tela-cliente-admin/tela-cliente-admin.component';
-
-
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'agendamento/novo/:id', component: AgendamentoComponent, canActivate: [AuthGuard] },
-  { path: 'agendamentos', component: AgendamentosComponent, canActivate: [AuthGuard] },
-  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] , data: {requiredRoles: ['admin']}},
-  { path: "agendamento/:id", component: UpdateAgendamentoComponent, canActivate: [AuthGuard] },
+  { path: 'agendamento/novo/:id', component: AgendamentoComponent},
+  { path: 'agendamentos', component: AgendamentosComponent },
+  { path: 'admin', component: AdminComponent, data: { requiredRoles: ['admin'] } },
+  { path: "agendamento/:id", component: UpdateAgendamentoComponent },
   { path: 'carregando', component: SpinnerComponent },
   { path: 'cadastro', component: CadastroComponent },
   {
-    path: 'telaBarbeiro', component: TelaBarbeiroComponent, canActivate: [AuthGuard],
+    path: 'telaBarbeiro', component: TelaBarbeiroComponent,
     children: [{ path: 'serv', component: ServicoComponent },
-    { path: 'agend', component: AgendamentosComponent, canActivate: [AuthGuard] },
-    { path: 'servlist', component: ServicoListBarbeiroComponent, canActivate: [AuthGuard] }]
+    { path: 'agend', component: AgendamentosComponent, },
+    { path: 'servlist', component: ServicoListBarbeiroComponent, }]
   },
   {
     path: 'telaCliente', component: TelaClienteComponent,
-    children: [{ path: 'serv', component: ServicoComponent, canActivate: [AuthGuard] },
-    { path: 'agend', component: AgendamentosComponent, canActivate: [AuthGuard] }], canActivate: [AuthGuard]
+    children: [{ path: 'serv', component: ServicoComponent, },
+    { path: 'agendamento/novo/:id', component: AgendamentoComponent}],
   },
-  { path: 'portfolio', component: PortfolioComponent, canActivate: [AuthGuard] },
-  { path: 'servico', component: ServicoComponent, canActivate: [AuthGuard] },
-  { path: 'servicoList', component: ServicoListComponent},
-  { path: 'perfilCliente/:email', component: PerfilClienteComponent, canActivate: [AuthGuard] },
-  { path: 'telaBarbeiro/:email', component: TelaBarbeiroComponent, canActivate: [AuthGuard] },
+  { path: 'portfolio', component: PortfolioComponent, },
+  { path: 'servico', component: ServicoComponent, },
+  { path: 'servicoList', component: ServicoListComponent },
+  { path: 'perfilCliente/:email', component: PerfilClienteComponent, },
+  { path: 'telaBarbeiro/:email', component: TelaBarbeiroComponent, },
   { path: 'telaClienteAdmin', component: TelaClienteAdminComponent },
   { path: '**', component: NotFoundComponent }
 
